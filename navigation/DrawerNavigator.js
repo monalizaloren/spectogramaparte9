29d5d4a85860229feb21de75553ce7f56128f01e
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-18 17:09:31 -0300

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -17,7 +17,44 @@ export default class DrawerNavigator extends Component {
         };
     }
 
-   
+    componentDidMount() {
+        let theme;
+        firebase
+            .database()
+            .ref("/users/" + firebase.auth().currentUser.uid)
+            .on("value", function (snapshot) {
+                theme = snapshot.val().current_theme;
+            });
+        this.setState({ light_theme: theme === "light" ? true : false });
+    }
+
+    render() {
+        let props = this.props;
+        return (
+            <Drawer.Navigator
+                drawerContentOptions={{
+                    activeTintColor: "#e91e63",
+                    inactiveTintColor: this.state.light_theme ? "black" : "white",
+                    itemStyle: { marginVertical: 5 }
+                }}
+                drawerContent={props => <CustomSidebarMenu {...props} />}
+            >
+                <Drawer.Screen
+                    name="Tela Inicial"
+                    component={StackNavigator}
+                    options={{ unmountOnBlur: true }}
+                />
+                <Drawer.Screen
+                    name="Perfil"
+                    component={Profile}
+                    options={{ unmountOnBlur: true }}
+                />
+                <Drawer.Screen
+                    name="Logout"
+                    component={Logout}
+                    options={{ unmountOnBlur: true }}
+                />
+            </Drawer.Navigator>
         );
     }
 }
